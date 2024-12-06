7b9431e47af3e9de7db930bb53631a9aa14e4133
krazykarthik2 <goparajukarthik2@gmail.com>
2024-12-07 02:41:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='h5visualizer',
+    version='1.0.0',
+    py_modules=['h5visualizer'],
+    install_requires=[
+        'tensorflow',
+        'matplotlib',
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'h5visualizer=h5visualizer:main',  # Corrected entry point
+        ],
+    },
+)
